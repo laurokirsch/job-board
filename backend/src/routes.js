2467f255bdacde7317b74d82809e63ca61633566
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-04-28 20:50:44 -0300

    post and get /company endpoint created and querying the db

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/company', (req, res) => {
+routes.get('/company', async (req, res) => {
+  const companies = await connection('company').select('*');
+
+  return res.json(companies);
+});
+
+routes.post('/company', async (req, res) => {
   const { name, email, country, city, website } = req.body;
 
   const id = crypto.randomBytes(4).toString('HEX');
 
-  return res.json();
+  await connection('company').insert({
+    id,
+    name,
+    email,
+    country,
+    city,
+    website,
+  });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
