3f7ff931135872232faa3b39350f69de67d46235
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-05-01 19:30:56 -0300

    get jobs for specific company, delete job from board, logout function

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
 import './styles.css';
 
 import jobBoardImg from '../../assets/job-board.png';
 import logoImg from '../../assets/jobs-logo.jpg';
 
 const Logon = () => {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post('/sessions', { id });
+      const companyInfo = {
+        companyId: id,
+        companyName: res.data.name,
+      };
+      localStorage.setItem('companyInfo', JSON.stringify(companyInfo));
+      history.push('/profile');
+    } catch (error) {
+      console.log(error);
+      alert('Error while trying to login');
+    }
+  };
+
   return (
     <div className='logon-container'>
       <section className='form'>
         <img src={logoImg} alt='Hyper Jobs' />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Logon</h1>
-          <input placeholder='Your ID' />
+          <input
+            placeholder='Your ID'
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
           <button className='button' type='submit'>
             Login
           </button>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,70 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/jobs-logo.jpg';
 import './styles.css';
 
 const Profile = () => {
+  const [jobs, setJobs] = useState([]);
+  const storedCompany = JSON.parse(localStorage.getItem('companyInfo'));
+
+  const { companyName, companyId } = storedCompany;
+
+  const history = useHistory();
+
+  useEffect(() => {
+    (async function getJobs() {
+      const res = await api.get('company/jobs', {
+        headers: {
+          Authorization: companyId,
+        },
+      });
+      setJobs(res.data);
+    })();
+  }, [companyId]);
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`job/${id}`, {
+        headers: {
+          Authorization: companyId,
+        },
+      });
+      setJobs(jobs.filter((job) => job.id !== id));
+    } catch (error) {
+      alert('Error while trying to delete job');
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  const showLoading = () => <h2>Loading jobs...</h2>;
+
+  const showJobs = () => (
+    <ul>
+      {jobs.map((job) => {
+        const { id, title, description, tags, remote, country, city } = job;
+        return (
+          <li key={id}>
+            <strong>Title</strong>
+            <p>{title}</p>
+            <strong>Description:</strong>
+            <p>{description}</p>
+            <strong>Tags</strong>
+            <p>{tags}</p>
+            <strong>Remote</strong>
+            <p>{remote ? 'Yes' : 'No'}</p>
+            <button type='button' onClick={() => handleDelete(id)}>
+              <FiTrash2 size={20} color='#a8a8b3' />
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
   return (
     <div className='profile-container'>
       <header>
         <img src={logoImg} alt='Hyper Jobs' />
-        <span>Welcome, Company</span>
+        <span>Welcome, {companyName}</span>
         <Link to='job/new' className='button' to='job/new'>
           Post new job
         </Link>
-        <button type='button'>
+        <button type='button' onClick={handleLogout}>
           <FiPower size={18} color='#e02041' />
         </button>
       </header>
       <h1>Latest jobs</h1>
-      <ul>
-        <li>
-          <strong>Job title</strong>
-          <p>Remote or not</p>
-          <strong>Description:</strong>
-          <p>Description test</p>
-          <strong>E-mail</strong>
-          <p>E-mail test</p>
-          <button type='button'>
-            <FiTrash2 size={20} color='#a8a8b3' />
-          </button>
-        </li>
-        <li>
-          <strong>Job title</strong>
-          <p>Remote or not</p>
-          <strong>Description:</strong>
-          <p>Description test</p>
-          <strong>E-mail</strong>
-          <p>E-mail test</p>
-          <button type='button'>
-            <FiTrash2 size={20} color='#a8a8b3' />
-          </button>
-        </li>
-        <li>
-          <strong>Job title</strong>
-          <p>Remote or not</p>
-          <strong>Description:</strong>
-          <p>Description test</p>
-          <strong>E-mail</strong>
-          <p>E-mail test</p>
-          <button type='button'>
-            <FiTrash2 size={20} color='#a8a8b3' />
-          </button>
-        </li>
-        <li>
-          <strong>Job title</strong>
-          <p>Remote or not</p>
-          <strong>Description:</strong>
-          <p>Description test</p>
-          <strong>E-mail</strong>
-          <p>E-mail test</p>
-          <button type='button'>
-            <FiTrash2 size={20} color='#a8a8b3' />
-          </button>
-        </li>
-      </ul>
+      {jobs.length ? showJobs() : showLoading()}
     </div>
   );
 };
