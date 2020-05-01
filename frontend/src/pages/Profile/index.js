ec8037a729365af811edd9d2dcc2d700085a26e5
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-05-01 13:12:22 -0300

    finish job listing for specific company

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import logoImg from '../../assets/jobs-logo.jpg';
+import './styles.css';
+
+const Profile = () => {
+  return (
+    <div className='profile-container'>
+      <header>
+        <img src={logoImg} alt='Hyper Jobs' />
+        <span>Welcome, Company</span>
+        <Link className='button' to='job/new'>
+          Post new job
+        </Link>
+        <button type='button'>
+          <FiPower size={18} color='#e02041' />
+        </button>
+      </header>
+      <h1>Latest jobs</h1>
+      <ul>
+        <li>
+          <strong>Job title</strong>
+          <p>Remote or not</p>
+          <strong>Description:</strong>
+          <p>Description test</p>
+          <strong>E-mail</strong>
+          <p>E-mail test</p>
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>Job title</strong>
+          <p>Remote or not</p>
+          <strong>Description:</strong>
+          <p>Description test</p>
+          <strong>E-mail</strong>
+          <p>E-mail test</p>
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>Job title</strong>
+          <p>Remote or not</p>
+          <strong>Description:</strong>
+          <p>Description test</p>
+          <strong>E-mail</strong>
+          <p>E-mail test</p>
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>Job title</strong>
+          <p>Remote or not</p>
+          <strong>Description:</strong>
+          <p>Description test</p>
+          <strong>E-mail</strong>
+          <p>E-mail test</p>
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon/';
 import Register from './pages/Register/';
+import Profile from './pages/Profile/';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/' component={Logon} exact />
       <Route path='/register' component={Register} />
+      <Route path='/profile' component={Profile} />
     </Switch>
   </BrowserRouter>
 );
