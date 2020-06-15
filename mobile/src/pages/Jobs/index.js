cf14af6d47278987a90c93b6964695d5e26b1761
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-06-14 21:47:52 -0300

    fetch jobs from backend, infinite scroll, deeplink to mail and whatsapp

--- mobile/src/pages/Jobs/index.js
+++ mobile/src/pages/Jobs/index.js
@@ -1,48 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import api from '../../services/api';
+
 import { Feather } from '@expo/vector-icons';
-import WhiteText from '../../components/WhiteText';
-
 import logoImg from '../../assets/logo.png';
-
 import styles from './styles';
 
 const Jobs = () => {
+  const [jobs, setJobs] = useState([]);
+  const [totalJobs, setTotalJobs] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
-  const navigateToSingle = () => {
-    navigation.navigate('Single');
+  const navigateToSingle = (job) => {
+    navigation.navigate('Single', { job });
   };
+
+  async function loadJobs() {
+    if (loading) return;
+    if (totalJobs > 0 && jobs.length === totalJobs) return;
+
+    setLoading(true);
+    try {
+      const response = await api.get('api/job', { params: { page } });
+      setJobs([...jobs, ...response.data.jobs]);
+      setTotalJobs(response.headers['x-total-count']);
+      setPage(page + 1);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadJobs();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image style={styles.image} source={logoImg} />
-        <Text style={styles.headerTextBold}>327 jobs found</Text>
+        <Text style={styles.headerTextBold}>{totalJobs} jobs found</Text>
       </View>
-      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.title}>Welcome to Web Scraped Jobs!</Text>
       <Text style={styles.description}>
-        This is the mobile version of the job board
+        The most recent opportunities are listed below
       </Text>
       <FlatList
-        data={[1, 2, 3, 4, 5]}
+        data={jobs}
         style={styles.jobsList}
-        keyExtractor={(data) => String(data)}
+        keyExtractor={(job) => String(job.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        onEndReached={loadJobs}
+        onEndReachedThreshold={0.2}
+        renderItem={({ item: job }) => (
           <View style={styles.job}>
             <Text style={styles.jobProperty}>Title</Text>
-            <Text style={styles.jobValue}>alsjkdf</Text>
+            <Text style={styles.jobValue}>{job.title}</Text>
             <Text style={styles.jobProperty}>Description</Text>
-            <Text style={styles.jobValue}>asfasfs</Text>
+            <Text style={styles.jobValue}>{job.description}</Text>
             <Text style={styles.jobProperty}>Tags</Text>
-            <Text style={styles.jobValue}>fasdfsfas</Text>
+            <Text style={styles.jobValue}>{job.tags}</Text>
             <Text style={styles.jobProperty}>Remote</Text>
-            <Text style={styles.jobValue}>asfsafs</Text>
+            <Text style={styles.jobValue}>{job.remote}</Text>
             <Text style={styles.jobProperty}>Location</Text>
-            <Text style={styles.jobValue}>asdfasfs</Text>
+            <Text style={styles.jobValue}>
+              {job.country} - {job.city}
+            </Text>
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={navigateToSingle}
+              onPress={() => navigateToSingle(job)}
             >
               <Text style={styles.detailsButtonText}>More details</Text>
               <Feather name='arrow-right' size={16} color='darkblue' />
--- mobile/src/pages/Single/index.js
+++ mobile/src/pages/Single/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+
 import { Feather } from '@expo/vector-icons';
-import { View, TouchableOpacity, Image, Text } from 'react-native';
-
 import logoImg from '../../assets/logo.png';
-
 import styles from './styles';
-import { useNavigation } from '@react-navigation/native';
 
 const Single = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const job = route.params.job;
+  const message = `Hello, ${job.name} I would like to apply to the current opening for ${job.title}`;
+  const phone = '';
+
   const navigateBack = () => {
     navigation.goBack();
   };
+
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      subject: `New application: ${job.title}`,
+      recipients: [job.email],
+      body: message,
+    });
+  };
+
+  const sendWhatsApp = () => {
+    Linking.openURL(`whatsapp://send?phone=+${phone}&text=${message}`);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -23,23 +42,25 @@ const Single = () => {
       </View>
       <View style={styles.job}>
         <Text style={styles.jobProperty}>Title</Text>
-        <Text style={styles.jobValue}>alsjkdf</Text>
+        <Text style={styles.jobValue}>{job.title}</Text>
         <Text style={styles.jobProperty}>Description</Text>
-        <Text style={styles.jobValue}>asfasfs</Text>
+        <Text style={styles.jobValue}>{job.description}</Text>
         <Text style={styles.jobProperty}>Tags</Text>
-        <Text style={styles.jobValue}>fasdfsfas</Text>
+        <Text style={styles.jobValue}>{job.tags}</Text>
         <Text style={styles.jobProperty}>Remote</Text>
-        <Text style={styles.jobValue}>asfsafs</Text>
+        <Text style={styles.jobValue}>{job.remote}</Text>
         <Text style={styles.jobProperty}>Location</Text>
-        <Text style={styles.jobValue}>asdfasfs</Text>
+        <Text style={styles.jobValue}>
+          {job.country} - {job.city}
+        </Text>
       </View>
       <View style={styles.contactBox}>
         <Text style={styles.callToAction}>Apply for this job now</Text>
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
