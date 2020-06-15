7def16b6438c00964b4740a54888f71609ecc04c
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-06-15 01:15:23 -0300

    finishes api routes validation with celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
 
 app.set('base', '/api');
 app.use(cors());
-app.use(express.json()); // automatically converts every requisition body to json
+app.use(errors());
+app.use(express.json());
 app.use('/api', routes);
 
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,20 +1,82 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const routes = express.Router();
 
 const CompanyController = require('./controllers/CompanyController');
 const CompanyProfileController = require('./controllers/CompanyProfileController');
 const JobController = require('./controllers/JobController');
 const SessionController = require('./controllers/SessionController');
+const { join } = require('./database/connection');
 
-routes.post('/sessions', SessionController.create);
+routes.post(
+  '/sessions',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+  }),
+  SessionController.create
+);
 
 routes.get('/company', CompanyController.index);
-routes.post('/company', CompanyController.create);
 
-routes.get('/company/jobs', CompanyProfileController.index);
+routes.post(
+  '/company',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      country: Joi.string().required(),
+      city: Joi.string().required(),
+      website: Joi.string().required(),
+    }),
+  }),
+  CompanyController.create
+);
 
-routes.get('/job', JobController.index);
-routes.post('/job', JobController.create);
-routes.delete('/job/:id', JobController.delete);
+routes.get(
+  '/company/jobs',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  CompanyProfileController.index
+);
+
+routes.get(
+  '/job',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  JobController.index
+);
+routes.post(
+  '/job',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      tags: Joi.string().required(),
+      remote: Joi.bool().required(),
+    }),
+  }),
+  JobController.create
+);
+routes.delete(
+  '/job/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  JobController.delete
+);
 
 module.exports = routes;
